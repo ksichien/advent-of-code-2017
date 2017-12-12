5a734cb6132536cfc81561f8c7005ea5649cbd7a
K Sichien <ksichien@posteo.net>
2017-12-12 10:27:48 +0100

    refactoring

--- day-3/part1.ps1
+++ day-3/part1.ps1
@@ -1,35 +1,30 @@
 function spiral ([int]$data) {
-    $rootofbound = [Math]::Ceiling([Math]::Sqrt($data)) # find the nearest higher perfect square root
-    $bound = $rootofbound*$rootofbound # square the root to a number
-    if ($rootofbound % 2 -eq 0 -and $bound -ne $data) {$stepsofbound = $rootofbound + 1} else {$stepsofbound = $rootofbound} # make sure the root is an uneven number
-    $bound = $stepsofbound * $stepsofbound # square the result to a number
-    $stepsofbound -= 1 # remove 1 from the root to find the manhattan distance
+    $rootofbound = [Math]::Ceiling([Math]::Sqrt($data)) # find the nearest perfect square root
+    if ($rootofbound % 2 -eq 0) {$rootofbound += 1} # make sure the root is an uneven number
+    $bound = $rootofbound * $rootofbound # square the root to find the boundary
+    $stepsofbound = $rootofbound - 1 # subtract 1 from the root to find the manhattan distance
 
-    # find the numbers in the corners of the spiral
+    # find the numbers in the corners of the spiral based on the boundary
     $bottomleftcorner = $bound - $stepsofbound
     $topleftcorner = $bottomleftcorner - $stepsofbound
     $toprightcorner = $topleftcorner - $stepsofbound
-    $bottomrightcorner = $toprightcorner - ($stepsofbound-1) # 1 step less because otherwise the array's last element overlaps with the bound
+    $bottomrightcorner = $toprightcorner - ($stepsofbound-1) # 1 step less because otherwise the array's last element overlaps with the boundary
 
-    # generate arrays based on the corner numbers
-    $bottomarray = $bottomleftcorner..$bound
-    $leftarray = $topleftcorner..$bottomleftcorner
-    $toparray = $toprightcorner..$topleftcorner
-    $rightarray = $bottomrightcorner..$toprightcorner
-
-    # calculate the steps based on which array the data is a member of
-    if ($bottomarray -contains $data) { steps $bottomarray $data $stepsofbound }
-    elseif ($leftarray -contains $data) { steps $leftarray $data $stepsofbound }
-    elseif ($toparray -contains $data) { steps $toparray $data $stepsofbound }
-    elseif ($rightarray -contains $data) { steps $rightarray $data $stepsofbound }
-    else { "Something went wrong :(" }
+    # calculate the steps based on which two corners the data is between
+    switch ($true) {
+        ($data -in $bottomleftcorner..$bound) { steps @($bottomleftcorner..$bound) $data $stepsofbound; break }
+        ($data -in $topleftcorner..$bottomleftcorner) { steps @($topleftcorner..$bottomleftcorner) $data $stepsofbound; break }
+        ($data -in $toprightcorner..$topleftcorner) { steps @($toprightcorner..$topleftcorner) $data $stepsofbound; break }
+        ($data -in $bottomrightcorner..$toprightcorner) { steps @($bottomrightcorner..$toprightcorner) $data $stepsofbound; break }
+        default { "Something went wrong :(" }
+    }
 }
 
-function steps ([array]$array, [int]$data, [int]$stepsofbound) {
-    if ($array.indexof($data) -ge $array.length/2) {
+function steps ([array]$array, [int]$data, [int]$stepsofbound) { # determine which corner the data is closer to
+    if ($array.indexof($data) -ge $array.length/2) { # if closer to the upper corner, subtract their indexes to find the diffference
         $difference = ($array.length-1) - $array.indexof($data)
     }
-    else {
+    else { # if closer to the bottom corner, the data's index is equal to their difference
         $difference = $array.indexof($data)
     }
     write-host ($stepsofbound - $difference)
@@ -39,4 +34,4 @@ spiral '1' # 0
 spiral '12' # 3
 spiral '23' # 2
 spiral '1024' # 31
-spiral (get-content './input.txt')
+spiral (get-content './input.txt') # 480
